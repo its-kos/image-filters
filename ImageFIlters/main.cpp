d8def887c2fd421a561bf32fcf077a1bcb250f95
Konstantinos Katserelis <45661771+its-kos@users.noreply.github.com>
2022-10-12 18:31:41 +0300

    Arguments done, Documentation 90% complete

--- ImageFIlters/main.cpp
+++ ImageFIlters/main.cpp
@@ -15,15 +15,15 @@ int main(int argc, char* argv[]) {
 	/*	
 	 *	Handling command line options
 	 *	Available options:
-	 *		[-h| --help]	   Display help menu
-	 *		[-v| --version]	   Display version information
-	 *		[-i| --image]	   User-supplied image (Required)
-	 *		[-f| --filter]	   User-supplied filter (Optional)
-	 *		[-p| --parameters] Filter parameters (Required)
-	 *		[-l| --loglevel]   User-supplied logging level (Optional)
+	 *		[-h| --help]	   Display help menu			(Optional, Overrides any other option)
+	 *		[-v| --version]	   Display version information	(Optional, Overrides any other option)
+	 *		[-i| --image]	   User-supplied image			(Required)
+	 *		[-f| --filter]	   User-supplied filter			(Optional)
+	 *		[-p| --parameters] Filter parameters			(Required)
+	 *		[-l| --loglevel]   User-supplied logging level	(Optional)
 	 */
 
-	// Adding all the available options
+	//	Creating the description object
 	po::options_description desc("Program options");
 	desc.add_options()
 
@@ -31,6 +31,9 @@ int main(int argc, char* argv[]) {
 		 *	Here, the operator `()` is overloaded, so
 		 *	calls to that operator add options to the 
 		 *	`options_description` object.
+		 * 
+		 *	- multitoken()		Supports multiple tokens for argument / Usually used with the std::vector<T> type
+		 *	- implicit_value()	The value given when the flag is provided but not the argument for that flag
 		 */
 		("help,h", "Display help menu")
 		("version,v", "Display program version")
@@ -40,12 +43,14 @@ int main(int argc, char* argv[]) {
 	    ("loglevel,l", po::value<unsigned int>(), "User-supplied logging level -- 0 = All (Default), 1 = Errors only, 2 = Warnings only (Optional)");
 
 
-	// Parsing the options from the command line and storing them 
-	// in a `variables_map m`
+	//	Parsing the options from the command line and storing them 
+	//	in a `variables_map` m
 	po::variables_map vm;
 	po::store(po::parse_command_line(argc, argv, desc), vm);
 	po::notify(vm);
 
+	//	If no flag is given or the `help` argument is given,
+	//	we print the help menu
 	if ((vm.empty() == true) || (vm.count("help"))) {
 		std::cout << desc << std::endl;
 		return 0;
@@ -56,15 +61,22 @@ int main(int argc, char* argv[]) {
 		return 0;
 	}
 	
-	// TODO: Implement multiple image processing
+	/***	TODO(1): Implement multiple image processing				***/
+	/***	TODO(2): Implement support for other image formats			***/
+	/***	TODO(3): Implement functionality for choosing a log-level 	***/
+
+	//	Checking that the image flag was provided,
+	//	and that we have at least 1 image argument
 	if (vm.count("image") && (vm["image"].as<std::vector<std::string>>().size() >= 1)) {
 
-		// Right now we dont support multiple images
+		//	THIS IF BLOCK OF CODE IS TEMPORARY UNTIL WE COMPLETE TODO(1)
 		if (vm["image"].as<std::vector<std::string>>().size() > 1) { 
 			std::cout << "Right now only 1 image at the time is supported" << std::endl;
 			return 0;
 		}
 
+		//	We need to check that the image is a PPM image
+		//	Until TODO(2) is done
 		imagePath = vm["image"].as<std::vector<std::string>>()[0];
 		if (imagePath.find(".ppm") == std::string::npos) {
 			std::cerr << "[Error] " << imagePath << " is not a valid `PPM` image" << std::endl;
@@ -76,11 +88,25 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 
+	/***	TODO(4): Implement support for other filters	***/
+
 	if (vm.count("filter")) {
 		std::string filter = vm["filter"].as<std::string>();
+
+		if (filter.empty()) {
+			std::cerr << "[Error] filter flag (-f) provided but no filter chosen." << std::endl;
+			return 1;
+		}
+
+		//	If the filter is not `Linear` or `Gamma` we exit
+		//	Until TODO(4) is completed
 		if (filter == "Linear" || filter == "linear" || filter == "LINEAR") {
+
+			//	If a filter is chosed but either no arguments or the wrong amount
+			//	for that filter are provided, we exit the program
 			if (vm.count("parameter")) {
 				std::vector<float> arg_vec = vm["parameter"].as<std::vector<float>>();
+
 				if (arg_vec.size() == 6) {
 
 					Image passedImage, newImage;
@@ -106,6 +132,7 @@ int main(int argc, char* argv[]) {
 					passedImage.load(imagePath, "ppm");
 					newImage = Image(passedImage);
 
+					//	Appending `filtered_` in front of the new image name
 					const int pos = imagePath.find_last_of('\\');
 					if (pos == -1) {
 						const int pos = imagePath.find_last_of('/');
@@ -117,6 +144,7 @@ int main(int argc, char* argv[]) {
 					} else {
 						imagePath.insert(pos, "filtered_");
 					}
+
 					std::cout << "Saving image: " + imagePath << std::endl;
 					newImage.save(imagePath, "ppm");
 
@@ -129,7 +157,7 @@ int main(int argc, char* argv[]) {
 		} else if (filter == "Gamma" || filter == "gamma" || filter == "GAMMA") {
 			if (vm.count("parameter")) {
 				std::vector<float> arg_vec = vm["parameter"].as<std::vector<float>>();
-				std::cout << arg_vec.size() << std::endl;
+
 				if (arg_vec.size() == 1) {
 
 					Image passedImage, newImage;
